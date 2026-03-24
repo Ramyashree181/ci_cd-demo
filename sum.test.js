4cd13efcbff51918c6960f42ebb2219f97163f32
Ramyashree181 <ramyagowda0703@gmail.com>
2026-03-24 17:43:35 +0530

    Add demo files

---
+++ sum.test.js
@@ -0,0 +1,11 @@
+import sum from './sum';
+
+describe("test for sum funx", () =>{
+    test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(3);
+   });
+
+    test('adds 1 + 5 to equal 6', () => {
+    expect(sum(1,5)).toBe(6);
+   });
+})
